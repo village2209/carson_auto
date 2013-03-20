42d36752a69317c41f034ee286d2f95a559a3bde
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-03-20 17:14:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarsonAuto::Application.config.secret_token = 'c447cbdb6c5352d46c9505755d28186a2e7fb7b1addbd55facce33b85d2375ea0c6bcc0fa59c03ceda20b71f12d39ff7ce62ff004e4bd3bba7a87248ee660489'
