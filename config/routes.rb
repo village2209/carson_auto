6e5994f0cbea2f525e43894f9eeca0a59d23f26b
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-03-24 12:09:28 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 CarsonAuto::Application.routes.draw do
+
+  resources :users
+  
+  root to: "static_pages#home"
+
+  match '/home', to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
+
+ 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
