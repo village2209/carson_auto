500fd1c873a16c6d2acdd6a551680dcfbd8e3d66
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-03-25 10:54:49 +0200

    Add car_image model

--- app/models/car.rb
+++ app/models/car.rb
@@ -21,6 +21,6 @@
 class Car < ActiveRecord::Base
   belongs_to :admin
   belongs_to :invoice_line
-  has_many :images
+  has_many :car_images
   attr_accessible :color, :fueltype, :image_desc, :image_file, :make, :mileage, :model, :price, :transmission, :year
 end
