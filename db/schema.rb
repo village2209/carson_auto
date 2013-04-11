5b61eb62f23a843803b8855c35e6560027540d2f
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-04-11 12:31:36 +0200

    Make user model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325085055) do
+ActiveRecord::Schema.define(:version => 20130411101158) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -157,8 +157,9 @@ ActiveRecord::Schema.define(:version => 20130325085055) do
     t.string   "password"
     t.string   "country"
     t.string   "address"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
