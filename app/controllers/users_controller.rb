66e1e9fb69e39886befb6cc8b007cca717174f7c
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-05-01 23:27:48 +0200

    Finish registration confirmation email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 		
 		if @user.save
+			UserMailer.registration_confirmation(@user).deliver
 			sign_in @user
 			redirect_to(root_path)
 			flash[:success] = "Welcome to Carson App"
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+	:address    		=> "smtp.gmail.com",
+	:port				=> 587,
+	:domain				=> "gmail.com",
+	:user_name 			=> "village2209@googlemail.com",
+	:password   		=> "$Vil2290",
+	:authentication 	=> "plain",
+	:enable_starttls	=> true
+}
