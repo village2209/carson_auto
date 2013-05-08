f109eab39b12b51681d310b396e6b20bfb6fb650
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-05-08 20:24:06 +0200

    Migrate active admin users

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130508173812) do
 
-
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
     t.string   "resource_type", :null => false
@@ -42,11 +41,20 @@ ActiveRecord::Schema.define(:version => 20130508173812) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "car_images", :force => true do |t|
+    t.integer  "car_id"
+    t.string   "image_desc"
+    t.binary   "image_file"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "car_images", ["car_id"], :name => "index_car_images_on_car_id"
 
   create_table "cars", :force => true do |t|
     t.string   "make"
